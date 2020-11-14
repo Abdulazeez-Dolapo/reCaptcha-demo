bdb5772affe14ee477e4df2a028e7e185bf07d81
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-11-14 22:34:25 +0100

    create contact form and add recaptcha

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,62 +1,80 @@
-
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
+    "@nuxtjs/dotenv",
+    "@nuxtjs/proxy",
+    [
+      "@nuxtjs/recaptcha",
+      {
+        /* reCAPTCHA options */
+        hideBadge: false, // Boolean Hide badge element (v3 & v2 via size=invisible)
+        language: "English", // String Recaptcha language (v2)
+        siteKey: process.env.SITE_KEY, // String Site key for requests
+        version: 3, // Number Version
+        size: "normal" // String Size: 'compact', 'normal', 'invisible' (v2)
+      }
+    ]
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {
+    proxy: true
+  },
+  proxy: {
+    "/captcha-api/": {
+      target: "https://www.google.com/recaptcha/api",
+      pathRewrite: {
+        "^/captcha-api": ""
+      }
+    }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,189 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        recaptcha-demo
-      </h1>
-      <h2 class="subtitle">
-        A demo for the integration of Google reCaptcha in your nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div class="contact-form">
+      <h2>Contact Us</h2>
+
+      <form @submit.prevent="onSubmit">
+        <input
+          autocomplete="off"
+          placeholder="Email"
+          type="email"
+          v-model="email"
+        />
+
+        <input
+          autocomplete="off"
+          placeholder="Phone number"
+          type="text"
+          v-model="phoneNumber"
+        />
+
+        <textarea
+          autocomplete="off"
+          placeholder="Enter your message"
+          type="text"
+          v-model="message"
+          rows="4"
+        />
+
+        <button type="submit" v-if="!loading">Send Message</button>
+
+        <p v-else>Loading...</p>
+      </form>
+
+      <div class="message" v-if="notificationMessage">
+        <p class="notification-text" :style="errorStatus ? 'color: red;' : ''">
+          {{ notificationMessage }}
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  async mounted() {
+    // Initialize recaptcha instance
+    try {
+      await this.$recaptcha.init();
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  data() {
+    return {
+      email: "",
+      phoneNumber: "",
+      message: "",
+      notificationMessage: "",
+      errorStatus: false,
+      loading: false
+    };
+  },
+  watch: {
+    notificationMessage(newValue) {
+      // check if the new notification message is not of a truthy value
+      if (!Boolean(newValue)) return;
+
+      // Clear notification message after 5 seconds
+      const context = this;
+      setTimeout(() => {
+        context.notificationMessage = "";
+      }, 5000);
+    }
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        this.loading = true;
+
+        // Start the verification process
+        const response = await this.verifyCaptcha();
+
+        // Display error message if verification was not successful
+        if (!response.success) {
+          this.$recaptcha.reset();
+          this.loading = false;
+          this.errorStatus = true;
+          this.notificationMessage =
+            "There was an error with your reCaptcha verification. Please try again.";
+          return;
+        }
+
+        // If verification was successful, send the message
+        await this.sendMessage();
+
+        this.errorStatus = false;
+        this.notificationMessage =
+          "Thank you for reaching out. We will get back to you as soon as possible";
+
+        this.loading = false;
+        this.$recaptcha.reset();
+      } catch (error) {
+        this.loading = false;
+        console.log(error);
+      }
+    },
+    async verifyCaptcha() {
+      try {
+        const token = await this.$recaptcha.execute();
+
+        const response = await this.$axios.$post(
+          `/captcha-api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`
+        );
+
+        return response;
+      } catch (error) {
+        this.loading = false;
+        return error;
+      }
+    },
+    async sendMessage() {
+      // The send message logic goes in here
+    }
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  justify-content: center;
   align-items: center;
+  background-color: whitesmoke;
+}
+
+.contact-form {
+  background-color: white;
+  width: 30%;
+  margin: 0 auto;
+  box-shadow: 1px 2px 6px grey;
+  padding: 15px;
+}
+
+h2 {
   text-align: center;
+  padding-bottom: 10px;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+input,
+textarea {
+  width: 100%;
+  margin-bottom: 10px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+input,
+button {
+  height: 30px;
 }
 
-.links {
-  padding-top: 15px;
+button {
+  text-align: center;
+  width: 40%;
+  background-color: green;
+  color: white;
+  border: none;
+  margin-bottom: 10px;
+}
+
+button:hover {
+  cursor: pointer;
+  width: 41%;
+  height: 31px;
+}
+
+.message {
+  border: solid 1px grey;
+  min-height: 30px;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+}
+
+.notification-text {
+  font-size: 14px;
 }
 </style>
